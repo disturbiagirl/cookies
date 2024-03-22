dafb2c20d9b5e95ab2617d0618380acc114a43fd
Anastasia <122746649+disturbiagirl@users.noreply.github.com>
2024-03-23 00:03:20 +0100

    make login and logout functionality

--- src/actions.ts
+++ src/actions.ts
@@ -1,7 +1,12 @@
+"use server";
 import { getIronSession } from "iron-session";
 import { sessionOptions, SessionData } from "../src/lib";
 import { cookies } from "next/headers";
 import { defaultSession } from "@/lib";
+import { redirect } from "next/navigation";
+
+let username = "John";
+let isPro = true;
 
 export const getSession = async () => {
   const session = await getIronSession<SessionData>(cookies(), sessionOptions);
@@ -12,6 +17,32 @@ export const getSession = async () => {
   return session;
 };
 
-export const login = async () => {};
+export const login = async (formData: FormData) => {
+  const session = await getSession();
 
-export const logout = async () => {};
+  const formUsername = formData.get("username") as string;
+  const formPassword = formData.get("password") as string;
+
+  // Check user in the db
+  // const user = await.db.getUser({username,password})
+
+  if (formUsername !== username) {
+    return {
+      error: "wrong credentials",
+    };
+  }
+
+  session.userId = "1";
+  session.username = formUsername;
+  session.isPro = isPro;
+  session.isLoggedIn = true;
+
+  await session.save();
+  redirect("/");
+};
+
+export const logout = async () => {
+  const session = await getSession();
+  session.destroy();
+  redirect("/");
+};
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,10 @@
+import LoginForm from "@/components/loginForm";
+
 export default function Login() {
   return (
     <div className="login">
       <h1>Welcome to the Login page</h1>
+      <LoginForm />
     </div>
   );
 }
