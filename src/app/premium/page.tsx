4ecfd0f43bd0b44e95399e9b6d8524eb6a5d1946
Anastasia <122746649+disturbiagirl@users.noreply.github.com>
2024-03-23 23:31:40 +0100

    protect Premium page content with session

--- src/app/premium/page.tsx
+++ src/app/premium/page.tsx
@@ -1,8 +1,35 @@
-export default function Premium() {
+import { getSession } from "@/actions";
+import { redirect } from "next/navigation";
+import Link from "next/link";
+
+const PremiumPage = async () => {
+  const session = await getSession();
+
+  if (!session.isLoggedIn) {
+    redirect("/");
+  }
+
+  if (!session.isPro) {
+    return (
+      <div className="notPremium">
+        <h1>Only premium users can see the content!</h1>
+        <Link href="/profile">
+          Go to the profile page to upgrade to premium
+        </Link>
+      </div>
+    );
+  }
+
   return (
     <div className="premium">
-      <h1>Welcome to the premium page</h1>
-      <h1>Only premium users can see the content!</h1>
+      <h1>Welcome to the PremiumPage</h1>
+      <ul>
+        <li>Apple</li>
+        <li>Orange</li>
+        <li>Peach</li>
+      </ul>
     </div>
   );
-}
+};
+
+export default PremiumPage;
